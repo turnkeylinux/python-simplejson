e113920fcb2b95283a50c04b7c9b95ceacd1b228
SVN-Git Migration <python-modules-team@lists.alioth.debian.org>
2015-10-08 13:48:57 -0700

    Imported Upstream version 2.3.3

--- simplejson/encoder.py
+++ simplejson/encoder.py
@@ -170,8 +170,8 @@ class JSONEncoder(object):
         self.use_decimal = use_decimal
         self.namedtuple_as_object = namedtuple_as_object
         self.tuple_as_array = tuple_as_array
-        if isinstance(indent, (int, long)):
-            indent = ' ' * indent
+        if indent is not None and not isinstance(indent, basestring):
+            indent = indent * ' '
         self.indent = indent
         if separators is not None:
             self.item_separator, self.key_separator = separators
