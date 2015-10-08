5ad58fee28a95ed9627c9d3b62f36511791d60f0
SVN-Git Migration <python-modules-team@lists.alioth.debian.org>
2015-10-08 13:48:30 -0700

    Imported Upstream version 1.4

--- simplejson/encoder.py
+++ simplejson/encoder.py
@@ -3,11 +3,11 @@ Implementation of JSONEncoder
 """
 import re
 
-# this should match any kind of infinity
-INFCHARS = re.compile(r'[infINF]')
 ESCAPE = re.compile(r'[\x00-\x19\\"\b\f\n\r\t]')
-ESCAPE_ASCII = re.compile(r'([\\"]|[^\ -~])')
+ESCAPE_ASCII = re.compile(r'([\\"/]|[^\ -~])')
 ESCAPE_DCT = {
+    # escape all forward slashes to prevent </script> attack
+    '/': '\\/',
     '\\': '\\\\',
     '"': '\\"',
     '\b': '\\b',
@@ -20,27 +20,27 @@ for i in range(20):
     ESCAPE_DCT.setdefault(chr(i), '\\u%04x' % (i,))
 
 def floatstr(o, allow_nan=True):
-    s = str(o)
-    # If the first non-sign is a digit then it's not a special value
-    if (o < 0.0 and s[1].isdigit()) or s[0].isdigit():
-        return s
-    elif not allow_nan:
+    # Check for specials.  Note that this type of test is processor- and/or
+    # platform-specific, so do tests which don't depend on the internals.
+
+    # assume this produces an infinity on all machines (probably not guaranteed)
+    INFINITY = 1e66666
+
+    if o != o:
+        text = 'NaN'
+    elif o == INFINITY:
+        text = 'Infinity'
+    elif o == -INFINITY:
+        text = '-Infinity'
+    else:
+        return str(o)
+
+    if not allow_nan:
         raise ValueError("Out of range float values are not JSON compliant: %r"
             % (o,))
-    # These are the string representations on the platforms I've tried
-    if s == 'nan':
-        return 'NaN'
-    if s == 'inf':
-        return 'Infinity'
-    if s == '-inf':
-        return '-Infinity'
-    # NaN should either be inequal to itself, or equal to everything
-    if o != o or o == 0.0:
-        return 'NaN'
-    # Last ditch effort, assume inf
-    if o < 0:
-        return '-Infinity'
-    return 'Infinity'
+
+    return text
+
 
 def encode_basestring(s):
     """
@@ -91,7 +91,7 @@ class JSONEncoder(object):
     """
     __all__ = ['__init__', 'default', 'encode', 'iterencode']
     def __init__(self, skipkeys=False, ensure_ascii=True,
-            check_circular=True, allow_nan=True, sort_keys=False):
+            check_circular=True, allow_nan=True, sort_keys=False, indent=None):
         """
         Constructor for JSONEncoder, with sensible defaults.
 
@@ -116,6 +116,11 @@ class JSONEncoder(object):
         If sort_keys is True, then the output of dictionaries will be
         sorted by key; this is useful for regression tests to ensure
         that JSON serializations can be compared on a day-to-day basis.
+
+        If ``indent`` is a non-negative integer, then JSON array
+        elements and object members will be pretty-printed with that
+        indent level.  An indent level of 0 will only insert newlines.
+        ``None`` is the most compact representation.
         """
 
         self.skipkeys = skipkeys
@@ -123,6 +128,13 @@ class JSONEncoder(object):
         self.check_circular = check_circular
         self.allow_nan = allow_nan
         self.sort_keys = sort_keys
+        self.indent = indent
+        self.current_indent_level = 0
+
+    def _newline_indent(self):
+        if self.indent is None:
+            return ''
+        return '\n' + (' ' * (self.indent * self.current_indent_level))
 
     def _iterencode_list(self, lst, markers=None):
         if not lst:
@@ -133,16 +145,19 @@ class JSONEncoder(object):
             if markerid in markers:
                 raise ValueError("Circular reference detected")
             markers[markerid] = lst
-        yield '['
+        self.current_indent_level += 1
+        newline_indent = self._newline_indent()
+        yield '[' + newline_indent
         first = True
         for value in lst:
             if first:
                 first = False
             else:
-                yield ', '
+                yield ', ' + newline_indent
             for chunk in self._iterencode(value, markers):
                 yield chunk
-        yield ']'
+        self.current_indent_level -= 1
+        yield self._newline_indent() + ']'
         if markers is not None:
             del markers[markerid]
 
@@ -155,7 +170,9 @@ class JSONEncoder(object):
             if markerid in markers:
                 raise ValueError("Circular reference detected")
             markers[markerid] = dct
-        yield '{'
+        self.current_indent_level += 1
+        newline_indent = self._newline_indent()
+        yield '{' + newline_indent
         first = True
         if self.ensure_ascii:
             encoder = encode_basestring_ascii
@@ -190,12 +207,13 @@ class JSONEncoder(object):
             if first:
                 first = False
             else:
-                yield ', '
+                yield ', ' + newline_indent
             yield encoder(key)
             yield ': '
             for chunk in self._iterencode(value, markers):
                 yield chunk
-        yield '}'
+        self.current_indent_level -= 1
+        yield self._newline_indent() + '}'
         if markers is not None:
             del markers[markerid]
 
